a8d52f77a93a7323ef2c9359708d4e2d43db4711
Utschie <littlecat.j@hotmail.com>
2023-10-26 01:31:09 +0200

    data read from the USBVCom

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,6 +1,11 @@
 #include <ros/ros.h>
 #include <serial/serial.h>
 #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+using namespace ros;
 
 int main(int argc, char** argv)
 {
@@ -50,7 +55,48 @@ int main(int argc, char** argv)
             uint8_t buffer[280];
             //读出数据
             n = sp.read(buffer, n);
+            if (buffer[0]==97 && buffer[1]==99 && buffer[2]==99) {
+                vector<uint8_t> vbuffer(buffer, buffer + sizeof(buffer));//array to vector for convenience
+                string sAcc_x(vbuffer.begin() + 8, vbuffer.begin() + 17);
+                string sAcc_y(vbuffer.begin() + 21, vbuffer.begin() + 30);
+                string sAcc_z(vbuffer.begin() + 34, vbuffer.begin() + 43);
+                string sOmega_x(vbuffer.begin() + 54, vbuffer.begin() + 63);
+                string sOmega_y(vbuffer.begin() + 67, vbuffer.begin() + 76);
+                string sOmega_z(vbuffer.begin() + 80, vbuffer.begin() + 89);
+                string sQuaternion_w(vbuffer.begin() + 105, vbuffer.begin() + 114);
+                string sQuaternion_x(vbuffer.begin() + 118, vbuffer.begin() + 127);
+                string sQuaternion_y(vbuffer.begin() + 131, vbuffer.begin() + 140);
+                string sQuaternion_z(vbuffer.begin() + 144, vbuffer.begin() + 153);
+                string sWheelspeed_LF(vbuffer.begin() + 170, vbuffer.begin() + 179);
+                string sWheelspeed_RF(vbuffer.begin() + 184, vbuffer.begin() + 193);
+                string sWheelspeed_LB(vbuffer.begin() + 198, vbuffer.begin() + 207);
+                string sWheelspeed_RB(vbuffer.begin() + 212, vbuffer.begin() + 221);
+                string sVx(vbuffer.begin() + 229, vbuffer.begin() + 238);
+                string sVy(vbuffer.begin() + 243, vbuffer.begin() + 252);
+                string sVz(vbuffer.begin() + 257, vbuffer.begin() + 266);
+                float fAcc_x = stof(sAcc_x);
+                float fsAcc_y = stof(sAcc_y);
+                float fAcc_z = stof(sAcc_z);
+                float fOmega_x = stof(sOmega_x);
+                float fOmega_y = stof(sOmega_y);
+                float fOmega_z = stof(sOmega_z);
+                float fQuaternion_w = stof(sQuaternion_w);
+                float fQuaternion_x = stof(sQuaternion_x);
+                float fQuaternion_y = stof(sQuaternion_y);
+                float fQuaternion_z = stof(sQuaternion_z);
+                float fWheelspeed_LF = stof(sWheelspeed_LF);
+                float fWheelspeed_RF = stof(sWheelspeed_RF);
+                float fWheelspeed_LB = stof(sWheelspeed_LB);
+                float fWheelspeed_RB = stof(sWheelspeed_RB);
+                float fVx = stof(sVx);
+                float fVy = stof(sVy);
+                float fVz = stof(sVz);
+                std::cout <<std::fixed<<std::setprecision(6) << fAcc_x << " ";
+            }
 
+
+
+            /*
             for(int i=0; i<n; i++)
             {
                 //16进制的方式打印到屏幕
@@ -58,6 +104,8 @@ int main(int argc, char** argv)
                 std::cout<<buffer[i];
 
             }
+             */
+            std::cout << n << " ";
             std::cout << std::endl;
         }
         loop_rate.sleep();
